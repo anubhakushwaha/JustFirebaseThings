edb96605ea74c782778a7fb828f4f6bacb589639
anubhakushwaha <anu.kushwaha1211@gmail.com>
2016-04-28 22:55:20 +0530

    Trying out Firebase
    Checking out Firebase

---
+++ app/src/main/java/com/example/gisi/stillfirebase/Login.java
@@ -0,0 +1,61 @@
+package com.example.gisi.stillfirebase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class Login extends AppCompatActivity {
+
+    String Eusername;
+    String Epassword;
+    EditText username;
+    EditText password;
+    int flag=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+          username= (EditText) findViewById(R.id.editText);
+          password= (EditText) findViewById(R.id.editText2);
+
+        Firebase.setAndroidContext(this);
+    }
+
+    public void login(View v)
+    {
+        // Get a reference to our posts
+        Firebase ref = new Firebase("https://testinganu.firebaseio.com/");
+        // Attach an listener to read the data at our posts reference
+        Eusername=username.getText().toString();
+        Epassword=password.getText().toString();
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    String databaseuser=postSnapshot.child("name").getValue().toString();
+                    String databasepassword=postSnapshot.child("password").getValue().toString();
+                    if(Eusername.trim().equals(databaseuser)&&Epassword.trim().equals(databasepassword))
+                    {
+                        Toast.makeText(getApplicationContext(),"Viola correct password",Toast.LENGTH_LONG).show();
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag==0)
+                    Toast.makeText(getApplicationContext(),"Wrong password",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/gisi/stillfirebase/Signup.java
@@ -0,0 +1,54 @@
+package com.example.gisi.stillfirebase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class Signup extends AppCompatActivity {
+
+    EditText name,mail,password;
+    String Ename,Email,Epassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        name= (EditText) findViewById(R.id.editText3);
+        mail=(EditText) findViewById(R.id.editText4);
+        password=(EditText) findViewById(R.id.editText5);
+
+        Firebase.setAndroidContext(this);
+    }
+    public void done(View v)
+    {
+
+        final int[] currentusers = new int[1];
+
+        // Get a reference to our posts
+        Firebase ref = new Firebase("https://testinganu.firebaseio.com/no of users");
+        
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                currentusers[0] = (int) snapshot.child("no of users").getValue();
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+        Toast.makeText(getApplicationContext(),currentusers[0]+"",Toast.LENGTH_LONG).show();
+    }
+}
